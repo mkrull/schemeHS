dcfad3533b6aef89237915f6e9f6abc84f14ea54
Matthias Krull <m.krull@uninets.eu>
2014-10-10 16:08:52 -0700

    parse lists

--- src/SchemeHS/Parser.hs
+++ src/SchemeHS/Parser.hs
@@ -16,6 +16,7 @@ module SchemeHS.Parser (
     readExpr
 ) where
 
+import           Control.Monad
 import           SchemeHS.Parser.Bools
 import           SchemeHS.Parser.Numbers
 import           SchemeHS.Parser.Strings
@@ -40,6 +41,27 @@ parseExpr =  parseLispAtom
          <|> parseLispChar
          <|> parseLispNumber
          <|> parseLispBool
+         <|> parseQuoted
+         <|> do
+             char '('
+             x <- (try parseLispList) <|> parseLispDottedList
+             char ')'
+             return x
+
+parseLispList :: Parser LispVal
+parseLispList = liftM LispList $ sepBy parseExpr spaces
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ LispList [LispAtom "quote", x]
+
+parseLispDottedList :: Parser LispVal
+parseLispDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ LispDottedList head tail
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
--- src/SchemeHS/Parser/Types.hs
+++ src/SchemeHS/Parser/Types.hs
@@ -14,8 +14,6 @@
 
 module SchemeHS.Parser.Types where
 
-import           Text.Show
-
 data LispVal = LispAtom String
              | LispList [LispVal]
              | LispDottedList [LispVal] LispVal
